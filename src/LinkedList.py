6c839c89f6f8c4253b5fabac2b0e8235f378d58c
Raj janwa <rajjanwa@gmail.com>
2017-12-17 20:34:52 +0530

    Added Queue

--- src/LinkedList.py
+++ src/LinkedList.py
@@ -3,6 +3,7 @@ Created on Dec 17, 2017
 
 @author: rajjanwa
 '''
+from operator import pos
 
 class Node:
     def __init__(self,data):
@@ -48,16 +49,69 @@ class LinkedList:
             pos +=1
         return False      
 
-
-
+    def addAfterKey(self,key,data):
+        curr=self.head
+        while(curr != None) :
+            if curr.data == key :
+                new_node=Node(data)
+                new_node.next=curr.next
+                curr.next=new_node
+                break
+            curr=curr.next  
+    def getPosition(self,key):
+        curr=self.head
+        pos=1
+        while curr != None :
+            if curr.data == key:
+                return pos
+            pos += 1
+            curr=curr.next
+        return -1
+                
+    def remove(self,key):
+        preNodePosition=self.getPosition(key)-1
+        posCounter=1
+        curr=self.head
+        while curr != None :
+            if posCounter == preNodePosition :
+                temp=curr.next.next
+                curr.next=temp
+            curr=curr.next
+            posCounter += 1       
+                
+                
 if __name__ == '__main__':
     lst=LinkedList()
     lst.append(4)
     lst.append(412)
     lst.append(6)
     lst.append(41)
+    print"_________________________________________________"
+    
     lst.printList()
     lst.addAtStart(100)
     print str(lst.exists(101))
     print str(lst.exists(41))
+    
+    print"_________________________________________________"
+    
     lst.printList()    
+    
+    print"_________________________________________________"
+    lst.addAfterKey(5,60)
+    lst.printList()
+    
+    print"_________________________________________________"
+    lst.addAfterKey(6,60)
+    lst.printList()
+    
+    print"_________________________________________________"
+    
+    print str(lst.getPosition(6))
+    print str(lst.getPosition(8))
+    print"_________________________________________________"
+    print str(lst.remove(100))
+    lst.printList()
+    
+    print"_________________________________________________"
+    
---
+++ src/Queue.py
@@ -0,0 +1,62 @@
+'''
+Created on Dec 17, 2017
+
+@author: rajjanwa
+'''
+
+
+class Node:
+    def __init__(self,data=None):
+        self.data=data
+        self.next=None
+
+class Queue:
+    
+    def __init__(self):
+        self.head=None
+    
+    def isEmpty(self):
+        if self.head is None :
+            return True
+        else:
+            return False
+        
+    def addInQueue(self,data):
+        new_node=Node(data)
+        if self.head == None :
+            self.head=new_node
+        else:
+            curr=self.head
+            while curr.next != None:
+                curr=curr.next    
+            curr.next=new_node    
+    def display(self):
+        curr=self.head
+        index=0
+        while curr != None :
+            print "The queue value at index: "+str(index)+": "+str(curr.data)
+            curr=curr.next
+            index += 1
+            
+    def remove(self):
+        if self.isEmpty() :
+            print "Queue is emppty"
+        else:
+            temp=self.head.next
+            self.head=temp   
+            
+
+if __name__ == '__main__':
+    queue=Queue()
+    queue.addInQueue(23)
+    queue.addInQueue(3)
+    queue.addInQueue(2)
+    queue.addInQueue(13)
+    queue.addInQueue(233)
+    queue.addInQueue(234)
+    queue.addInQueue(236)
+    queue.addInQueue(2223)
+    queue.display()
+    queue.remove()
+    print "Queue after one remove"
+    queue.display()
